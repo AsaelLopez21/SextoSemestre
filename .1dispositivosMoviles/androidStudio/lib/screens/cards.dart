9b4ba4bc85083f4381a1886fad6f5b927008d333
AsaelLopez21 <asaellopezinfogmail.com>
2025-04-24 08:00:49 -0600

    cambios

--- .1dispositivosMoviles/androidStudio/lib/screens/cards.dart
+++ .1dispositivosMoviles/androidStudio/lib/screens/cards.dart
@@ -2,11 +2,79 @@ import 'package:flutter/material.dart';
 
 class CardScreen extends StatelessWidget {
   const CardScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Card screen')),
-      body: Center(child: Text('Card screen')),
+      body: ListView(children: [_customcard()]),
+    );
+  }
+}
+
+class _customcard extends StatelessWidget {
+  const _customcard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final primaryColor = Theme.of(context).iconTheme.color;
+
+    return Padding(
+      padding: const EdgeInsets.only(top: 1),
+      child: Card(
+        child: Column(
+          children: [
+            SizedBox(height: 10),
+            ListTile(
+              leading: Icon(
+                Icons.account_circle,
+                color: Theme.of(context).primaryIconTheme.color,
+              ),
+              title: Text(
+                "Nombre",
+                style: TextStyle(
+                  color: Theme.of(context).primaryIconTheme.color,
+                ),
+              ),
+              subtitle: Text(
+                "Puesto trabajo lorem ipsum dolor sit amet",
+                style: TextStyle(
+                  color: Theme.of(context).primaryIconTheme.color,
+                ),
+              ),
+            ),
+            //* otra parte
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(
+                    onPressed: null,
+                    child: Text(
+                      'Aceptar',
+                      style: TextStyle(
+                        color: Theme.of(context).primaryIconTheme.color,
+                      ),
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: null,
+                    child: Text(
+                      'Cancelar',
+                      style: TextStyle(
+                        color: Theme.of(context).primaryIconTheme.color,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 5),
+          ],
+        ),
+        color: primaryColor,
+      ),
     );
   }
 }
