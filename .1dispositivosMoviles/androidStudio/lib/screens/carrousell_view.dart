5168915d5fad3889151277ca8ba6808214a55028
AsaelLopez21 <asaellopezinfogmail.com>
2025-04-25 12:55:18 -0600

    finSemana

--- .1dispositivosMoviles/androidStudio/lib/screens/carrousell_view.dart
+++ .1dispositivosMoviles/androidStudio/lib/screens/carrousell_view.dart
@@ -4,10 +4,25 @@ class CarrousellViewX extends StatelessWidget {
   const CarrousellViewX({super.key});
   @override
   Widget build(BuildContext context) {
+    final size =
+        MediaQuery.of(context).size; //!obtener el tamaño de la pantalla
+
     return Scaffold(
       appBar: AppBar(title: Text('Carrousell view')),
       body: Center(
-        child: Text('Carrousell View'), //TODO: agregar el carrousel
+        child: Column(
+          children: [
+            //?Carrousel
+            Container(
+              height: size.height * 0.45,
+              width: double.infinity /*Ocupa todo el ancho*/,
+              color: Colors.amber,
+              child: Text('texto dentro container'),
+            ),
+
+            //?Otra cosa
+          ],
+        ),
       ),
     );
   }
